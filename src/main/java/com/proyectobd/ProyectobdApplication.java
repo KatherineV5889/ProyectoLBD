d57b5ddb32d5d720de1235d8e77da590d68b913e
LuisCarlosQuirosObando <lquiros460787@ufide.ac.cr>
2024-10-28 19:19:17 -0600

    Creacion del proyecto inicial

---
+++ src/main/java/com/proyectobd/ProyectobdApplication.java
@@ -0,0 +1,13 @@
+package com.proyectobd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectobdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectobdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectobd/ProyectobdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectobd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectobdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
